d6b854ef746dfaa390a4f4315b633c6b64860780
edgar <bj.illusion@hotmail.com>
2024-01-31 18:44:42 +0800

    Update stable_diffusion_service.py

--- stable_diffusion_service.py
+++ stable_diffusion_service.py
@@ -21,12 +21,9 @@ class StableDiffusionV2:
         )
         self.pipe = self.pipe.to("cuda")
 
-    async def __call__(self, request):
-        prompt = request.query_params.get("prompt", "")
-        img_size = int(request.query_params.get("img_size", 512))
-
+    async def __call__(self, prompt: str, img_size: int = 512):
         try:
-            image = await self.pipe.remote(prompt, img_size=img_size)
+            image = self.pipe(prompt, height=img_size, width=img_size).images[0]
             file_stream = BytesIO()
             image.save(file_stream, "PNG")
             return Response(content=file_stream.getvalue(), media_type="image/png")
@@ -38,7 +35,7 @@ serve.start()
 
 # Define the backend and endpoint.
 serve.create_backend("diffusion_model", StableDiffusionV2)
-serve.create_endpoint("diffusion_model", backend="diffusion_model", route="/diffusion_model")
+serve.create_endpoint("diffusion_model", backend="diffusion_model", route="/diffusion_model", methods=["GET", "POST"])
 
 # Use `block=False` to allow the script to run continuously.
 serve.run(host="0.0.0.0", port=8888, block=False)
