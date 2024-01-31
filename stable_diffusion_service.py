3814dad91bc0868b273d42f1e4f731dfd5a84ce5
edgar <bj.illusion@hotmail.com>
2024-01-31 18:36:28 +0800

    Update stable_diffusion_service.py

--- stable_diffusion_service.py
+++ stable_diffusion_service.py
@@ -22,25 +22,15 @@ class StableDiffusionV2:
         )
         self.pipe = self.pipe.to("cuda")
 
-    async def __call__(self, prompt: str, img_size: int = 512):
-        assert len(prompt), "prompt parameter cannot be empty"
-
-        with torch.autocast("cuda"):
-            image = self.pipe(prompt, height=img_size, width=img_size).images[0]
-            return image
-
-class Ingress:
     @serve.ingress(app)
     async def api_ingress(self, prompt: str, img_size: int = 512):
         try:
-            handle = serve.get_handle("diffusion_model")
-            image = await handle.remote(prompt, img_size=img_size)
+            image = await self.pipe.remote(prompt, img_size=img_size)
             file_stream = BytesIO()
             image.save(file_stream, "PNG")
             return Response(content=file_stream.getvalue(), media_type="image/png")
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
 
-Ingress()
-
+StableDiffusionV2()
 serve.run(host="0.0.0.0", port=8888)
