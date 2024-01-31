fd407377ace572fa02737b8c51a23957930b1060
edgar <bj.illusion@hotmail.com>
2024-01-31 17:25:52 +0800

    Update stable_diffusion_service.py

--- stable_diffusion_service.py
+++ stable_diffusion_service.py
@@ -122,4 +122,4 @@ if __name__ == "__main__":
     handle = serve.run(entrypoint)
 
     # Run FastAPI application using uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8888)
+    uvicorn.run(app, host="0.0.0.0", port=8888)
